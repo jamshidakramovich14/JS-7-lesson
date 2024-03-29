9e78d9b4b0efa7e4e69f960c7c516c6e48a8faf5
jamshidakramovich14 <jamshidturdiyev761@gmail.com>
2024-03-29 14:09:52 +0500

    7-lesson

---
+++ script.js
@@ -0,0 +1,85 @@
+// "use strict"
+
+
+
+// --------------------      14-masala  start   --------------------
+
+// 123 ->  312
+
+
+
+
+// let son=+prompt("Sonni kiritin");
+// let yuzlar=(son%1000-son%100)/100;
+// let onlar=(son%100-son%10)/10
+// let birlar=son%10
+// alert(birlar+(yuzlar+"")+onlar)
+
+
+// --------------------    14-masala end    --------------------
+
+
+
+
+
+// --------------------      15-masala   start  --------------------
+
+// 123 -> 213 
+
+
+// let son=+prompt("Sonni kiritin");
+// let yuzlar=(son%1000-son%100)/100;
+// let onlar=(son%100-son%10)/10
+// let birlar=son%10
+// alert(onlar +(yuzlar+"")+birlar)
+
+
+
+
+// --------------------      15-masala   end  --------------------
+
+
+
+
+
+
+
+
+
+// --------------------      16-masala   start  --------------------
+
+// 123 -> 132 
+
+
+
+// let son=+prompt("Sonni kiritin");
+// let yuzlar=(son%1000-son%100)/100;
+// let onlar=(son%100-son%10)/10
+// let birlar=son%10
+// alert(yuzlar+(birlar+"")+onlar )
+
+
+
+// --------------------      16-masala   end  --------------------
+
+
+
+
+
+
+
+
+
+// --------------------      17-masala   start  --------------------
+
+
+let raqam  = prompt("raqam kiriting");
+let yuzlik = Math.floor((raqam % 1000)/ 100);
+console.log(yuzlik)
+
+
+
+
+
+
+// --------------------      17-masala   end  --------------------
