107eb0087f79d46fc631f5c853bfa2033f32fd64
jamshidakramovich14 <jamshidturdiyev761@gmail.com>
2024-03-29 20:46:54 +0500

    changed

--- script.js
+++ script.js
@@ -73,9 +73,9 @@
 // --------------------      17-masala   start  --------------------
 
 
-let raqam  = prompt("raqam kiriting");
-let yuzlik = Math.floor((raqam % 1000)/ 100);
-console.log(yuzlik)
+// let raqam  = prompt("raqam kiriting");
+// let yuzlik = Math.floor((raqam % 1000)/ 100);
+// console.log(yuzlik)
 
 
 
@@ -83,3 +83,123 @@ console.log(yuzlik)
 
 
 // --------------------      17-masala   end  --------------------
+
+
+
+
+
+
+// --------------------      18-masala   start  --------------------
+
+
+
+// let raqam  = prompt("raqam kiriting");
+// let minglik = Math.floor((raqam % 10000)/ 100);
+// console.log(minglik)
+
+
+
+
+
+// --------------------      18-masala   end  --------------------
+
+
+
+
+
+
+
+
+// --------------------      19-masala   start  --------------------
+
+
+
+// let secund = prompt("sekund kiritilsin");
+// let minut = Math.floor(secund / 60);
+
+// console.log (minut + "o'tdi")
+
+
+
+
+// --------------------      19-masala   end  --------------------
+
+
+
+
+
+
+// --------------------      20-masala   start  --------------------
+
+
+
+
+// let secund = prompt("sekund kiritilsin");
+// let soat = Math.floor(secund / 3600);
+
+// console.log (soat + "o'tdi")
+
+
+
+
+// --------------------      20-masala   end  --------------------
+
+
+
+
+
+
+
+
+
+// --------------------      21-masala   start  --------------------
+
+
+
+
+// let secund = prompt("sekund kiritilsin");
+// let minut = Math.floor(secund / 60);
+// let qoldiqSecund = (secund % 60);
+
+
+// console.log (minut + "o'tdi" + qoldiqSecund  "secund");
+
+
+
+
+// --------------------      21-masala   end  --------------------
+
+
+
+
+
+
+
+
+
+
+// --------------------      22-masala   start  --------------------
+
+
+
+
+
+
+// --------------------      22-masala   end  --------------------
+
+
+
+
+
+
+
+
+// --------------------      23-masala   start  --------------------
+
+
+
+ 
+
+
+
+// --------------------      23-masala   end  --------------------
